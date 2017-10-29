efcd8d697984dc33852a0bc2bf3e65decc2fbdd4
Tomas Masnik <tomas.masnik@gmail.com>
2017-10-29 21:35:22 +0100

    build a simple wall

--- src/main/java/cz/luzanky/minecraft/zombie/ZombiePlugin.java
+++ src/main/java/cz/luzanky/minecraft/zombie/ZombiePlugin.java
@@ -9,6 +9,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Zombie;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.util.Vector;
 
 public class ZombiePlugin extends JavaPlugin {
     public static final Logger LOG = Logger.getLogger(ZombiePlugin.class.getName());
@@ -29,14 +30,15 @@ public class ZombiePlugin extends JavaPlugin {
 //                Zombie zombie = ja.getWorld().spawn(zombieLocation,Zombie.class);
 
                 LOG.info(playerLocation.toString());
-                LOG.info(playerLocation.getDirection().normalize().toString());
-                
+                Vector directionVector = playerLocation.getDirection().normalize();
+                Vector directionVectorNormalized = new Vector(valueOf(directionVector.getX()), valueOf(directionVector.getY()), valueOf(directionVector.getZ()));
+                LOG.info(directionVectorNormalized.toString());
+
                 for (int i = 0; i < 10; i++) {
-                    Location loc = playerLocation.clone().add(playerLocation.getDirection().normalize().multiply(i).setY(1));
+                    Location loc = playerLocation.clone().add(directionVectorNormalized.clone().multiply(i));
                     loc.getBlock().setType(Material.AIR);
-                    loc = playerLocation.clone().add(playerLocation.getDirection().normalize().multiply(i).setY(0));
+                    loc = loc.clone().add(0,1,0);
                     loc.getBlock().setType(Material.AIR);
-                    
                 }
                 
                 ja.sendMessage("Zombie vytvorena. Tak bacha :)");
@@ -44,9 +46,39 @@ public class ZombiePlugin extends JavaPlugin {
                 return true;
             }
         }
+
+         if (label.equalsIgnoreCase("zed")) {
+             if (sender instanceof Player) {
+                 Player ja = (Player) sender;
+                 final Location playerLocation = ja.getLocation();
+
+
+                 Vector directionVector = playerLocation.getDirection().normalize();
+                 Vector directionVectorNormalized = new Vector(valueOf(directionVector.getX()), valueOf(directionVector.getY()), valueOf(directionVector.getZ()));
+                 LOG.info(directionVectorNormalized.toString());
+                 Location initialLoc = playerLocation.clone().add(directionVectorNormalized);
+                 for (int i = 0; i < 10; i++) {
+                     Location blockLoc = initialLoc.clone().add(i, 0, 0);
+
+                     blockLoc.getBlock().setType(Material.GOLD_ORE);
+                 }
+
+                 ja.sendMessage("Zed postavena.");
+                 return true;
+             }
+         }
         return false;
     }
-    
-    
-    
+
+    private double valueOf(double x) {
+        if (x > 0.5) {
+            return 1;
+        }
+        if (x < -0.5) {
+            return -1;
+        }
+        return 0;
+    }
+
+
 }
